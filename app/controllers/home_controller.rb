45b1c17d9f27d8ff2eed0d8524bcf4c8cc77596d
Federico <federicogratti@gmail.com>
2016-10-31 18:47:01 -0300

    Fixed: many rows on home when a user has many debts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,8 @@ class HomeController < ApplicationController
   # GET /users
   def index
     @debts = Debt.where owner_id: current_user.id, paid: false
-    @user_debts = Debt.where user_id: current_user.id, paid: false
+    @user_debts = Debt.select('owner_id, SUM(amount) as amount').where(user_id: current_user.id, paid: false).group(:owner_id)
+    #@user_debts = Debt.where user_id: current_user.id, paid: false
   end
 
   private
